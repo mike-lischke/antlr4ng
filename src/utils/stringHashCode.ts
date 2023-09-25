7a68dd160ca7f004b613d048fa1c99081c09917c
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2023-09-25 09:07:14 +0200

    Remove unused export
    Signed-off-by: Oskar Damkjaer <oskar.damkjaer@gmail.com>

--- src/utils/stringHashCode.ts
+++ src/utils/stringHashCode.ts
@@ -4,7 +4,7 @@
  * can be found in the LICENSE.txt file in the project root.
  */
 
-export const stringSeedHashCode = Math.round(Math.random() * Math.pow(2, 32));
+const stringSeedHashCode = Math.round(Math.random() * Math.pow(2, 32));
 
 export const stringHashCode = (value: string): number =>  {
     if (!value) {
