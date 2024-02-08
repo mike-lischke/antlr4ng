26dc66068cad69d7a71225c6d8a29a49087a3f67
Mike Lischke <mike@lischke-online.de>
2024-02-08 18:49:40 +0100

    Another build fix.
    Signed-off-by: Mike Lischke <mike@lischke-online.de>

--- src/atn/SetTransition.ts
+++ src/atn/SetTransition.ts
@@ -4,28 +4,31 @@
  * can be found in the LICENSE.txt file in the project root.
  */
 
-// A transition containing a set of values.
 import { IntervalSet } from "../misc/IntervalSet.js";
 import { Token } from "../Token.js";
 import { ATNState } from "./ATNState.js";
 import { Transition } from "./Transition.js";
 import { TransitionType } from "./TransitionType.js";
 
+/** A transition containing a set of values. */
 export class SetTransition extends Transition {
     public set: IntervalSet;
 
     public constructor(target: ATNState, set: IntervalSet) {
         super(target);
-        if (set !== undefined && set !== null) {
+        if (set) {
             this.set = set;
         } else {
-            this.set = new IntervalSet();
-            this.set.addOne(Token.INVALID_TYPE);
+            this.set = IntervalSet.of(Token.INVALID_TYPE, Token.INVALID_TYPE);
         }
     }
 
     public get serializationType(): number {
         return TransitionType.SET;
+    }
+
+    public override get label(): IntervalSet {
+        return this.set;
     }
 
     public override matches(symbol: number, _minVocabSymbol: number, _maxVocabSymbol: number): boolean {
