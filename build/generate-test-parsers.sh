7987a8740519277ab47edb421d9f65c2e73f6fea
Mike Lischke <mike@lischke-online.de>
2024-03-02 16:25:31 +0100

    Make the cli 2.0 version a peer dep.
    Signed-off-by: Mike Lischke <mike@lischke-online.de>

--- build/generate-test-parsers.sh
+++ build/generate-test-parsers.sh
@@ -2,7 +2,7 @@
 printf "\x1b[1m\x1b[34mGenerating test parsers...\x1b[0m\n\n"
 
 # Temporarily copy the ANTLR4 jar to the antlr4ng-cli directory, to allow using a newer version, not yet published to npm.
-cp cli/antlr4-4.13.2-SNAPSHOT-complete.jar node_modules/antlr4ng-cli/antlr4-4.13.2-SNAPSHOT-complete.jar
+# cp cli/antlr4-4.13.2-SNAPSHOT-complete.jar node_modules/antlr4ng-cli/antlr4-4.13.2-SNAPSHOT-complete.jar
 
 java -jar cli/antlr4-4.13.2-SNAPSHOT-complete.jar -Dlanguage=TypeScript -visitor -Xexact-output-dir -o ./tests/generated ./tests/fixtures/grammars/*.g4
 java -jar cli/antlr4-4.13.2-SNAPSHOT-complete.jar -Dlanguage=TypeScript -o tests/benchmarks/generated -visitor -listener -Xexact-output-dir tests/benchmarks/MySQLLexer.g4 tests/benchmarks/MySQLParser.g4
